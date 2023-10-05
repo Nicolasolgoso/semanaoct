088d277e1497ed743c65dc7f23effacc7386861b
Nicolasolgoso <145667874+Nicolasolgoso@users.noreply.github.com>
2023-10-05 13:25:55 +0200

    calculate-object

---
+++ chatbot.py
@@ -0,0 +1,5 @@
+name = input("¿Como te llamas? ")
+texto_interpolado = f"Hola {name}, un gusto, ¿Dónde vives? "
+ciudad = input(texto_interpolado)
+comida = input(f"¿Cuál es tu comida favorita {name}? ")
+gusto = input(f"Con que tu comida favorita es {comida}, la típica")
